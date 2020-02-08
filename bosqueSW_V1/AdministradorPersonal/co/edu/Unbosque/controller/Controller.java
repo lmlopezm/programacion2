969a8992147d533b64740fd7d2f0206d3242f69e
Chechobuitrago <50975995+Chechobuitrago@users.noreply.github.com>
2020-02-07 23:36:48 -0500

    Update Controller.java

--- bosqueSW_V1/AdministradorPersonal/co/edu/Unbosque/controller/Controller.java
+++ bosqueSW_V1/AdministradorPersonal/co/edu/Unbosque/controller/Controller.java
@@ -3,9 +3,11 @@ package co.edu.Unbosque.controller;
 import java.awt.event.*;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 import co.edu.Unbosque.Model.Persona;
@@ -44,6 +46,23 @@ public class Controller implements ActionListener {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+		}
+
+	}
+
+	/**
+	 * Metodo que al pasarle por parametro un objeto persona lo escribe en el
+	 * archivo
+	 * 
+	 * @param persona
+	 */
+	public void escribirEnArchivo(Persona persona) {
+		try {
+			salida = new ObjectOutputStream(new FileOutputStream(archivo));
+			salida.writeObject(personas);
+			salida.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 
 	}
