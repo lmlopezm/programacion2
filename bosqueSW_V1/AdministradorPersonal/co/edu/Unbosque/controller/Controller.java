712f2b2b74cc160bd5617bf48a4a1bf4cd6fad28
Chechobuitrago <50975995+Chechobuitrago@users.noreply.github.com>
2020-02-08 10:17:00 -0500

    metodo AgregarComision

--- bosqueSW_V1/AdministradorPersonal/co/edu/Unbosque/controller/Controller.java
+++ bosqueSW_V1/AdministradorPersonal/co/edu/Unbosque/controller/Controller.java
@@ -1,11 +1,20 @@
 package co.edu.Unbosque.controller;
 
-import java.awt.event.*;
-import java.io.*;
-import java.util.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
+import co.edu.Unbosque.Model.Concomision;
 import co.edu.Unbosque.Model.Persona;
-import co.edu.Unbosque.view.*;
+import co.edu.Unbosque.view.View;
 
 public class Controller implements ActionListener {
 
@@ -61,14 +70,45 @@ public class Controller implements ActionListener {
 
 	}
 
-//	public boolean agregarPersona(String cedula, String nombre, String apellido, int anioIngreso, String genero,
-//			String telefono, String correo, String direccion) {
-//
-////		Persona nuevo =new Persona(cedula,nombre,apellido,anioIngreso,genero,telefono,correo,direccion);
-//			
-//			
-//		
-//	}
+	public boolean agregarComision(String cedula, String nombre, String apellido, int anioIngreso, String genero,
+			String telefono, String correo, String direccion, int Clientes, int ComisionxCliente) {
+
+		Concomision nuevo = new Concomision(IG.getPanAgregar().gettCedula().getText(),
+				IG.getPanAgregar().gettNombre().getText(), IG.getPanAgregar().gettApellido().getText(),
+				Integer.parseInt(IG.getPanAgregar().gettAnioIngreso().getText()),
+				IG.getPanAgregar().gettGenero().getText(), IG.getPanAgregar().gettTel().getText(),
+				IG.getPanAgregar().gettCorreo().getText(), IG.getPanAgregar().gettCorreo().getText(), 0, 0);
+		if (buscarProducto(cedula) == null) {
+			personas.add(nuevo);
+			JOptionPane.showMessageDialog(null, "El empleado con: " + cedula + " ha sido agregado", "Oficias el Bosque",
+					JOptionPane.INFORMATION_MESSAGE);
+			return true;
+		} else {
+			JOptionPane.showMessageDialog(null, "El empleado: " + cedula + " ya se encuentra agregado",
+					"Venta Producto TecnoShop", JOptionPane.WARNING_MESSAGE);
+			return false;
+		}
+
+	}
+
+	public Persona buscarProducto(String identificador) {
+		Persona encontrado = null;
+
+		if (!personas.isEmpty()) {
+			System.out.println(personas);
+			for (int i = 0; i < personas.size(); i++) {
+				if (personas.get(i).getTelefono().equals(identificador)) {
+					encontrado = personas.get(i);
+					JOptionPane.showMessageDialog(null, "Ingeniero: " + encontrado);
+				}
+			}
+		} else {
+			JOptionPane.showMessageDialog(null,
+					"el producto con el ID: " + identificador + " no se encuentra registrado");
+		}
+
+		return encontrado;
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -98,18 +138,16 @@ public class Controller implements ActionListener {
 		}
 		if (e.getActionCommand().equals("Guardar")) {
 //			Senior
-			Persona nuevo = new Persona(IG.getPanAgregar().gettCedula().getText(),
-					IG.getPanAgregar().gettNombre().getText(), IG.getPanAgregar().gettApellido().getText(),
-					Integer.parseInt(IG.getPanAgregar().gettAnioIngreso().getText()),
-					IG.getPanAgregar().gettGenero().getText(), IG.getPanAgregar().gettTel().getText(),
-					IG.getPanAgregar().gettCorreo().getText(), IG.getPanAgregar().gettDireccion().getText()) {
-				int Calcular_salario() {
-					int i = 0;
-					return i;
-				}
-			};
-
-			IG.getPanAgregar().gettNombre();
+//			Persona nuevo = new Persona(IG.getPanAgregar().gettCedula().getText(),
+//					IG.getPanAgregar().gettNombre().getText(), IG.getPanAgregar().gettApellido().getText(),
+//					Integer.parseInt(IG.getPanAgregar().gettAnioIngreso().getText()),
+//					IG.getPanAgregar().gettGenero().getText(), IG.getPanAgregar().gettTel().getText(),
+//					IG.getPanAgregar().gettCorreo().getText(), IG.getPanAgregar().gettDireccion().getText()) {
+//				int Calcular_salario() {
+//					int i = 0;
+//					return i;
+//				}
+//			};
 		}
 
 	}
