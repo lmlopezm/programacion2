4fa75263f03604d788681818ea932a3aa244b421
santigs440 <56047704+santigs440@users.noreply.github.com>
2020-02-07 23:12:06 -0500

    controlador

--- bosqueSW_V1/AdministradorPersonal/co/edu/Unbosque/controller/Controller.java
+++ bosqueSW_V1/AdministradorPersonal/co/edu/Unbosque/controller/Controller.java
@@ -1,15 +1,46 @@
 package co.edu.Unbosque.controller;
 
 import java.awt.event.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+
 
 import co.edu.Unbosque.view.*;
 
 public class Controller implements ActionListener{
-	
+	private static File archivo = new File("data/datos.dat");
+	private ObjectInputStream entrada;
 	private View IG;
 	
 	public Controller() {
+		if(!archivo.exists()) {
+			try {
+				archivo.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}else {
+			
 		
+			if(archivo.length()!=0){
+				
+					try {
+						
+						entrada=new ObjectInputStream(new FileInputStream(archivo));
+						
+						
+						//pr= (ArrayList<CongeladoPorAgua>) entrada.readObject();
+						
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						
+					}
+			}
+		}
 		IG = new View(this);
 		
 		IG.setVisible(true);
