059b7b299de3d7ae96abe55806bb051b32f6c756
Chechobuitrago <50975995+Chechobuitrago@users.noreply.github.com>
2020-02-07 23:14:12 -0500

    Constructor Serializacion

--- bosqueSW_V1/AdministradorPersonal/co/edu/Unbosque/controller/Controller.java
+++ bosqueSW_V1/AdministradorPersonal/co/edu/Unbosque/controller/Controller.java
@@ -1,31 +1,57 @@
 package co.edu.Unbosque.controller;
 
 import java.awt.event.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.util.ArrayList;
 
 import co.edu.Unbosque.view.*;
 
-public class Controller implements ActionListener{
-	
+public class Controller implements ActionListener {
+
 	private View IG;
-	
+	private ObjectInputStream entrada;
+	private ObjectOutput salida;
+	private File archivo = new File("data/datos.dat");
+
 	public Controller() {
-		
+
 		IG = new View(this);
-		
+
 		IG.setVisible(true);
-	
+//		if (archivo.exists()) {
+//			System.out.println("El archivo ya existe");
+//		} else {
+//			try {
+//				archivo.createNewFile();
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//		}
+//
+//		if (archivo.length() != 0) {
+//			try {
+//				entrada = new ObjectInputStream(new FileInputStream(archivo));
+//				personas = (ArrayList<Personal>) entrada.readObject();
+//			} catch (IOException | ClassNotFoundException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//
+//		}
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+
 		if (e.getActionCommand().equals(PanelAgregar.VOLVER)) {
-			
+
 		}
-		
-		
+
 	}
-	
-	
 
 }
